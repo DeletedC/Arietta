60ac0c53ec500d2d5d00af8f60b98ac2641879db
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-27 11:58:54 -0400

    Elimnated whitespace from jumbotron

--- views/components/Banner.jsx
+++ views/components/Banner.jsx
@@ -3,9 +3,12 @@ const React= require('react');
 class Banner extends React.Component {
     render() {
         return (
-            <div className="jumbotron mb-0 text-center">
-                <h1 className="display-4">Piano Studio</h1>
-                <h1 className="display-4">Student Tracker</h1>
+            <div className="jumbotron jumbotron-fluid mb-0 text-center">
+                <div className="container">
+                    <h1 className="display-1">Arietta</h1>
+                    <h2>Studio Roster App</h2>
+                </div>
+                
             </div>
         );
     };
