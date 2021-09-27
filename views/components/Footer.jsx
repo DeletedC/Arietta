5be1eca9627a3331f91ee6b257fea23e75e300b7
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-27 01:07:08 -0400

    Added background image credits

--- views/components/Footer.jsx
+++ views/components/Footer.jsx
@@ -4,13 +4,19 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="container-fluid flex-grow-1 footer pt-5">
-                <div className="row justify-content-center">
-                    <p>Site by Curtis Woods</p>
-                </div>
-                <div className="row justify-content-center">
-                    <a href="https://www.CurtisJWoods.com">
-                        <p>See my portfolio</p>
+                <div className="d-flex flex-column align-items-center">
+                    <h2>Site by Curtis Woods</h2>
+                    <a href="https://www.CurtisJWoods.com" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                        <h3>See my portfolio</h3>
                     </a>
+                    <p>Music Pattern Vectors by <a href="https://www.vecteezy.com/free-vector/music-pattern" 
+                        target="_blank" 
+                        rel="noopener noreferrer">Vecteezy</a>, header background from <a href="https://pixabay.com/photos/piano-oldschool-vintage-music-3717165/"
+                        target="_blank"
+                        rel="noopener noreferrer">Pixabay</a>.
+                    </p>
                 </div>
 
             </div>
