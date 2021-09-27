aa7b7cd4bae210f8cbad17271edee79dbc66aa66
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-27 10:42:43 -0400

    Styled form in New.jsx

--- views/New.jsx
+++ views/New.jsx
@@ -11,13 +11,34 @@ class New extends React.Component {
                     </div>
                     <div className="row justify-content-center">
                         <form action="/students" method="POST">
-                            First Name: <input type="text" name="nameFirst"/><br/>
-                            Last Name: <input type="text" name="nameLast"/><br/>
-                            Picture URL: <input type="text" name="img"/><br/>
-                            Book: <input type="text" name="book"/><br/>
-                            Level: <input type="text" name="level"/><br/>
-                            Notes: <input type="text" name="notes"/><br/>
-                            <br/>
+                            <div className="form-row">
+                                <div className="col">
+                                    <input type="text" className="form-control" name="nameFirst" placeholder="First Name"/>
+                                </div>
+                                <div className="col">
+                                    <input type="text" className="form-control" name="nameLast" placeholder="Last Name"/>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <input type="text" className="form-control" name="img" placeholder="Picture URL"/>
+                                </div>
+                            </div>
+                            
+                            <div className="form-row">
+                                <div className="col">
+                                    <input type="text" className="form-control" name="book" placeholder="Book"/>
+                                </div>
+                                <div className="col">
+                                    <input type="text" className="form-control" name="level" placeholder="Level"/>
+                                </div>
+                            </div>
+                            
+                            <div className="row">
+                                <div className="col">
+                                    <input type="text" className="form-control" name="notes" placeholder="Notes"/>
+                                </div>
+                            </div>
                             <input className="btn btn-outline-success" type="submit" value="Submit"/>
                         </form>
                     </div>
