35e13b8378d079c6bddc05eaa82ae7377ca7d7c0
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-26 23:35:44 -0400

    Fixed the layout shift, long names now truncated

--- views/components/Card.jsx
+++ views/components/Card.jsx
@@ -7,10 +7,12 @@ class Card extends React.Component {
 
         return (
             <div className="col-md-3 col-sm-6 text-center mb-3 mt-3">
-                <a href={`/students/${student._id}`}>
-                    <h2>{student.nameLast}, {student.nameFirst}</h2>
+                    <a href={`/students/${student._id}`}>
+                        <h3 className="text-truncate">{student.nameLast},</h3>
+                        <h3 className="text-truncate">{student.nameFirst}</h3>
                     <img className="card-img-top img-thumbnail" src={student.img}></img>
-                </a>
+                    </a>
+         
                 <div className="row justify-content-center">
                     <form action={`/students/edit/${student._id}`} method="get">
                         <input className="btn btn-outline-secondary" type="submit" value="Edit"/>
