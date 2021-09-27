6b00557890b991336c6416bf7cda93170073e4eb
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-09-27 15:19:33 -0400

    Added Monsterrat and Merienda fonts

--- views/components/Layout.jsx
+++ views/components/Layout.jsx
@@ -9,16 +9,27 @@ class Layout extends React.Component {
             <html>
                 <head>
                     {/* meta tags for LinkedIn */}
-                    <meta property="og:title" content="Piano Studio Student Tracker"/>
+                    <meta property="og:title" content="Arietta"/>
                     <meta property="og:image" content="https://curtiswoods.herokuapp.com/img/screenshot.png"/>
-                    <meta property="og:description" content="Piano Studio Student Tracker"/>
+                    <meta property="og:description" content="Arietta, Studio Roster App"/>
                     <meta property="og:url" content="//curtiswoods.herokuapp.com/students"/>
 
                     {/* Bootstrap */}
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
                     
                     {/* Personal CSS */}
                     <link href="/css/style.css" rel="stylesheet"/>
+
+                    {/* Google Font, Merienda */}
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@700&display=swap" rel="stylesheet" />
+
+                    {/* Google Font, Montserrat */}
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+                    
                     <title>Student Tracker</title>
 
                     <link href="https://fonts.googleapis.com/css2?family=Chivo:ital,wght@0,700;0,900;1,400&display=swap" rel="stylesheet"/> 
